8d7ee94f4952c238267c69dd528098c800b35c59
godwin opara <godwinopara62@yahoo.com>
2021-11-19 22:11:14 +0400

    set up tailwind css and styled the nav

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      poppins: ["Poppins", "sans-serif"],
+    },
+    backgroundImage: {
+      desktop: "url(./src/images/bg-desktop.svg)",
+      mobile: "url(./src/images/bg-mobile.svg)",
+    },
+    extend: {
+      colors: {
+        purple: {
+          950: "#674baf",
+          970: "#e882e8",
+        },
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
